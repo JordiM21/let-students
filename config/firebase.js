fe2da2626a34841f9577cfd349de18f382751420
Jordi Mantilla <jordimantilla21@gmail.com>
2023-06-03 18:39:56 +0200

    gitignore and env

--- config/firebase.js
+++ config/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClZ328ZofnBfnJTGasYuQ8O8V3dFNexrM",
-  authDomain: "let-students.firebaseapp.com",
-  projectId: "let-students",
-  storageBucket: "let-students.appspot.com",
-  messagingSenderId: "29471672286",
-  appId: "1:29471672286:web:d453af022b2f973c8fff4f",
-  measurementId: "G-P7YLPBXYJS"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
