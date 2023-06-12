a1fa6c75ef78df7fbd320e6f305b969f903ae837
Jordi Mantilla <jordimantilla21@gmail.com>
2023-06-12 23:26:12 +0200

    fixed letters size mobile icons nav

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -40,15 +40,15 @@ export default function index() {
         <div className='bg-slate-100 z-50 bg-opacity-30 backdrop-blur-xl fixed py-2 px-2 justify-around rounded-lg bottom-3 md:hidden max-md:flex w-10/12'>
           <div onClick={() => router.push("/Dashboard")} className={`bg-red-500 group p-3 hover:shadow-md hover:shadow-red-900 cursor-pointer transition-all hover:scale-110 duration-150 active:scale-95 active:opacity-80 ease-in rounded-lg ${router.pathname == "/Dashboard" && "scale-110 -translate-y-2 border-2 opacity-90"}`}>
             <MdHome fill='white' className='mx-auto text-3xl' />
-            <p className='text-gray-300 text-[10px] absolute -bottom-0 opacity-0 group-hover:opacity-40 font-bold right-[13px]'>Home</p>
+            <p className='text-gray-300 text-[10px] absolute bottom-1 opacity-0 group-hover:opacity-60 font-bold right-[13px]'>Home</p>
           </div>
           <div onClick={() => router.push("/Niveles")} className={`bg-orange-500/95 group p-3 hover:shadow-md hover:shadow-orange-800 cursor-pointer transition-all hover:scale-110 duration-150 active:scale-95 active:opacity-80 ease-in rounded-lg ${router.pathname == "/Niveles" && "scale-110 -translate-y-2 border-2 opacity-90"}`}>
             <MdLibraryBooks fill='white' className='mx-auto text-3xl' />
-            <p className='text-gray-200 text-[10px] absolute bottom-0 opacity-0 group-hover:opacity-60 font-bold right-[9px]'>Lessons</p>
+            <p className='text-gray-200 text-[10px] absolute bottom-[3px] opacity-0 group-hover:opacity-60 font-bold right-[9px]'>Lessons</p>
           </div>
           <div onClick={() => router.push("/Immersive")} className={`bg-yellow-400/90 group p-3 hover:shadow-md hover:shadow-yellow-700 cursor-pointer transition-all hover:scale-110 duration-150 active:scale-95 active:opacity-80 ease-in rounded-lg ${router.pathname == "/Immersive" && "scale-110 -translate-y-2 border-2 opacity-90"}`}>
             <MdVideoLibrary fill='white' className='mx-auto text-3xl' />
-            <p className='text-gray-100 text-[9px] absolute bottom-[0.8px] opacity-0 group-hover:opacity-60 font-bold right-[4px]'>Immersive</p>
+            <p className='text-gray-100 text-[10px] absolute bottom-[3px] opacity-0 group-hover:opacity-60 font-bold right-[3px]'>Immersive</p>
           </div>
           <div className='bg-green-500/90 group p-3 hover:shadow-md hover:shadow-green-900 cursor-pointer transition-all hover:scale-110 duration-150 active:scale-95 active:opacity-80 ease-in rounded-lg'>
             <MdAnalytics fill='white' className='mx-auto text-3xl' />
