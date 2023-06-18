38b30cbc4d60d55c3ce1f1d21ac9acab50955e31
Jordi Mantilla <jordimantilla21@gmail.com>
2023-06-18 13:07:42 +0200

    create intermediate pages till 10

--- pages/Niveles/Intermediate/index.js
+++ pages/Niveles/Intermediate/index.js
@@ -64,10 +64,10 @@ export default function index() {
         )
       }
       <BackHeader largeTitle="Intermediate" parentTitle="Levels" />
-      <div className='md:w-2/5 max-md:w-10/12 mx-8 md:fixed bg-[var(--bluebg)]'>
+      <div className='md:w-2/5 max-md:w-10/12 mx-8 my-4 md:fixed bg-[var(--bluebg)]'>
         <Image src={image1} className='w-full h-48 md:h-80 object-cover rounded-md' />
-        <h3 className='text-2xl font-bold text-white'>Curso básico de inglés para los que están empezando.</h3>
-        <p>El curso básico de inglés, está diseñado para los que están empezando. Al finalizar el curso, el estudiante tendrá una comprensión de los conceptos básicos de inglés y será capaz de formar construcciones y oraciones simples.</p>
+        <h3 className='text-2xl font-bold text-white'>Curso intermedio de inglés para los que dominan los conceptos básicos del lenguaje.</h3>
+        <p>El curso intermedio es para los que dominan los conceptos básicos del lenguaje. Al finalizar este curso, el estudiante será capaz de entender y formar oraciones más complejas y comunicarse en inglés con mayor confianza.</p>
       </div>
       <div className='md:ml-[46%] max-md:w-10/12 mx-auto md:w-1/2 space-y-4'>
         <div className='space-y-2'>
@@ -97,7 +97,7 @@ export default function index() {
                 }
                 {
                   progress < index && (
-                    <div onClick={() => router.push(`/Niveles/${data.level}/${data.number}`)} className='grayscale opacity-70 transition-all 1s ease-in cursor-pointer flex justify-between items-center bg-gray-300 px-4 py-2 rounded-md'>
+                    <div className='grayscale opacity-70 transition-all 1s ease-in cursor-pointer flex justify-between items-center bg-gray-300 px-4 py-2 rounded-md'>
                       <div className='w-4/5'>
                         <small className='text-xs text-[var(--color3)] font-semibold'>UNIT {data.number}</small>
                         <p className='font-bold text-[var(--color2)]'>{data.title}</p>
