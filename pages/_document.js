884848c0c26b98bb3afe40931c750630658eb407
Jordi Mantilla <jordimantilla21@gmail.com>
2023-06-19 19:02:27 +0200

    clarity

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title> LET Students | Official Students Platform </title>
+          <link rel="icon" href="./Icon.png" />
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                  c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "hbpq3zkd07");
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
