f050cb405486013eafd1cd0ebfbc901dcb1da4c9
JordiM21 <jordimantilla21@gmail.com>
2025-11-07 13:27:30 +0100

    liked videos fixed

--- pages/likedVideos.jsx
+++ pages/likedVideos.jsx
@@ -25,16 +25,16 @@ const likedVideos = ({ likedVideos, userData, submit, setSubmit }) => {
     e.preventDefault()
     const { id } = userMatched
     try {
-        toast.error('Este video ya no te gusta!');
-        const userRef = doc(db, 'users', id);
-        const indexToRemove = likedVideosRef.current.findIndex((act) => act.id === video.id);
-        if (indexToRemove !== -1) {
-      likedVideosRef.current.splice(indexToRemove, 1);
-      await updateDoc(userRef, { likedVideos: likedVideosRef.current });
-      likedVideosRef.current = [...likedVideosRef.current];
-      setSubmit((prevSubmit) => !prevSubmit);
-    } else {
-        toast.error('Video not found in likedVideos array.');
+      toast.error('Este video ya no te gusta!')
+      const userRef = doc(db, 'users', id)
+      const indexToRemove = likedVideosRef.current.findIndex((act) => act.id === video.id)
+      if (indexToRemove !== -1) {
+        likedVideosRef.current.splice(indexToRemove, 1)
+        await updateDoc(userRef, { likedVideos: likedVideosRef.current })
+        likedVideosRef.current = [...likedVideosRef.current]
+        setSubmit((prevSubmit) => !prevSubmit)
+      } else {
+        toast.error('Video not found in likedVideos array.')
       }
     } catch (error) {
       console.error('Error al eliminar la actividad:', error)
@@ -53,7 +53,7 @@ const likedVideos = ({ likedVideos, userData, submit, setSubmit }) => {
           <div className=" hover:opacity-80 h-[220px] w-[400px] bg-[var(--blueSuperDark)] transition-all 1s ease-in cursor-pointer mx-auto rounded-md relative my-4">
             <div
               onClick={() => router.push(`/immersiveActivities/${video.id}`)}
-              className="bg-gray-200 h-[220px] absolute z-20 w-full opacity-80"
+              className="bg-gray-200 h-[220px] absolute z-20 w-full opacity-0"
             ></div>
             <ReactPlayer
               width={'90%'}
